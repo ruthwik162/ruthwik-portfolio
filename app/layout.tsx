1468235ef565ced089c99d094177f374b4e58969
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2026-05-15 15:29:07 +0530

    change in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "./Components/SmoothScroll";
 import Navbar from "./Components/Navbar";
-import PreloaderDiagonal from "./Components/Preloaderdiagonal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
